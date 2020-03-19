99f88a04dc8e5bffeeb3e081d9301ff3f16ffbcd
Takahiro Umeda <takahiro.umeda@pixta.co.jp>
2020-03-19 11:37:56 +0900

    センテンスタイトルロジックを追加

---
+++ sentence_title/evaluate_titles.rb
@@ -0,0 +1,76 @@
+require "csv"
+require_relative "./tokenize.rb"
+
+$is_debug = true
+
+def main(title_file, output_file)
+  output_file_writer = CSV.open(output_file, "w")
+  output_cols = ["title", "end_with_noun?", "with_space?", "only_one_noun?", "with_descriptive_noun?", "with_particle?", "with_relative?", "words"]
+  output_file_writer.puts(output_cols)
+
+  FileHandler.file_foreach(title_file) do |title|
+    tokenized_title = Tokenize::Tokenizer.exec(title)
+    next if tokenized_title.nil?
+    review_result = Tokenize::SentenceReviewer.exec(tokenized_title)
+    output_file_writer.puts(output_cols.map {|col| review_result[col]})
+  end
+
+  output_file_writer.close
+end
+
+module FileHandler
+  class << self
+    def file_foreach(file)
+      File.open(file) do |lines|
+        log "#{Time.now}: read start #{file}"
+        all_line_count = line_count(file)
+
+        return_values = lines.each_line.each.with_index(1).map do |line, line_no|
+          log progress(line_no, all_line_count) if progress_timing?(all_line_count, line_no)
+          yield(line.chomp)
+        end
+
+        log "#{Time.now}: read end #{file}"
+
+        return_values
+      end
+    end
+
+    def line_count(file)
+      open(file){|f|
+        while f.gets; end
+        f.lineno
+      }
+    end
+
+    private
+
+    def log(message)
+      puts(message) if $is_debug
+    end
+
+    def progress_timing?(all_line_count, line_no)
+      return false if all_line_count < 100
+
+      # NOTE 処理時間によって変更
+      div_number = 100
+
+      percent_unit = all_line_count / div_number
+      line_no % percent_unit == 0
+    end
+
+    def progress(current_count, all_count)
+      "#{Time.now}: #{CommonUtilities.percent(current_count, all_count)}% (#{current_count}/#{all_count})"
+    end
+  end
+end
+
+module CommonUtilities
+  class << self
+    def percent(num, all_count)
+      (num.fdiv(all_count) * 100).round(2)
+    end
+  end
+end
+
+main(ARGV[0], ARGV[1])
---
+++ sentence_title/tokenize.rb
@@ -0,0 +1,151 @@
+require "natto"
+
+module Tokenize
+  class Tokenizer
+    @@natto_mecab = Natto::MeCab.new
+
+    class << self
+      def exec(sentence)
+        words = []
+        target_sentence = normalize(sentence)
+        @@natto_mecab.parse(target_sentence) do |node|
+          next if node.is_eos?
+          words << TokenizedWord.new(node)
+        end
+        return if words.empty?
+        TokenizedSentence.new(target_sentence, words)
+      end
+
+      private
+
+      def normalize(original_str)
+        str = original_str.dup
+        str.gsub!(/（/, "(")
+        str.gsub!(/）/, ")")
+        str.gsub!(/　/, " ")
+        str.gsub!(/"/, "")
+        str
+      end
+    end
+  end
+
+  class TokenizedSentence
+    attr_reader :words, :score
+
+    def initialize(sentence, words)
+      @sentence = sentence
+      @words = words
+    end
+
+    def to_s
+      @sentence
+    end
+
+    def specified_part_of_speech_size(part_of_speech)
+      @words.select {|word| word.send("#{part_of_speech}?")}.size
+    end
+
+    def not_specified_part_of_speech_size(part_of_speech)
+      @words.reject {|word| word.send("#{part_of_speech}?")}.size
+    end
+
+  end
+
+  class TokenizedWord
+    attr_reader :original_word, :part_of_speech
+
+    PARTS_OF_SPEECH = {
+      NOUN: "名詞",
+      VERB: "動詞",
+      ADJECTIVE: "形容詞",
+      ADJECTIVE_VERB: "形容動詞",
+      RENTAISHI: "連体詞",
+      ADVERB: "副詞",
+      CONJUNCTION: "接続詞",
+      INTERJECTION: "感動詞",
+      PARTICLE: "助詞",
+      AUXILIARY_VERB: "助動詞",
+      SYMBOL: "記号",
+      ARTICLE: "冠詞",
+      PREPOSITION: "前置詞",
+      INTERROGATIVE: "疑問詞",
+    }
+
+    PARTS_OF_SPEECH.each do |pos_en, pos_ja|
+      define_method("#{pos_en.to_s.downcase}?") do
+        @part_of_speech == pos_ja
+      end
+    end
+
+    def initialize(natto_node)
+      @original_word = natto_node.surface
+      @tokenize_result_str = natto_node.feature
+      tokenize_results = natto_node.feature.split(",")
+
+      @part_of_speech = tokenize_results[0]
+    end
+
+    def to_s
+      @original_word
+    end
+  end
+
+  class SentenceReviewer
+    SPACE_SENTENCE_SCORE = -1
+    NOT_END_WITH_NOUN_SCORE = -2
+    ONLY_ONE_NOUN = -3
+
+    class << self
+      def exec(tokenized_sentence)
+        @sentence = tokenized_sentence
+        result = {
+          "title" => tokenized_sentence.to_s,
+          # "words" => @sentence.words.map {|word| [word.to_s, word.part_of_speech]}.to_h
+        }
+        [
+          "end_with_noun?", "with_space?", "only_one_noun?", "with_descriptive_noun?", "with_particle?", "with_relative?"
+        ].each_with_object(result) do |check_method, result_hash|
+          result_hash[check_method] = send(check_method) ? 1 : 0
+        end
+      end
+
+      private
+
+      def end_with_noun?
+        @sentence.words.last.noun?
+      end
+
+      # bad
+      def with_space?
+        !@sentence.to_s.match(/ /).nil?
+      end
+
+      def only_one_noun?
+        @sentence.not_specified_part_of_speech_size("noun") == 0
+      end
+
+      # good
+      def with_descriptive_noun?
+        end_with_noun? && @sentence.not_specified_part_of_speech_size("noun") > 0
+      end
+
+      def with_particle?
+        @sentence.specified_part_of_speech_size("particle") > 0
+      end
+
+      # 関係詞(動詞を含む)
+      def with_relative?
+        @sentence.specified_part_of_speech_size("verb") > 0
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  # デバッグコード
+  sentence = ARGV[0]
+
+  tokenized_sentence = Tokenize::Tokenizer.exec(sentence)
+  review_result = Tokenize::SentenceReviewer.exec(tokenized_sentence)
+  p review_result
+end
